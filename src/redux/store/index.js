e8b504f3a9e2278678a45aea1b5718329b4248ff
Jesse Galetta <jesse.galetta@gmail.com>
2026-05-29 21:46:07 +0200

    refactor: clear application state on LOGIN_START to prevent stale data conflicts

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,7 +5,7 @@ import usersReducer from "../reducers/usersReducer";
 import ticketsReducer from "../reducers/ticketsReducer";
 import bachelorsReducer from "../reducers/bachelorsReducer";
 import coursesReducer from "../reducers/coursesReducer";
-import { LOGOUT } from "../actions/auth";
+import { LOGOUT, LOGIN_START } from "../actions/auth";
 
 const appReducer = combineReducers({
   auth: authReducer,
@@ -17,8 +17,8 @@ const appReducer = combineReducers({
 });
 
 const rootReducer = (state, action) => {
-  if (action.type === LOGOUT) {
-    // Clear all state on logout to avoid stale errors like "Access Denied"
+  if (action.type === LOGOUT || action.type === LOGIN_START) {
+    // Clear all state on logout or when a new login starts to avoid stale errors like "Access Denied"
     state = undefined;
   }
   return appReducer(state, action);
